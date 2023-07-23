a39fe62a7fdd57338d8ea55fa3a749ade2d93689
arefins56 <arefins56@penguin>
2023-07-24 01:31:26 +0600

    frontend updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footer'>
-      Made with React by Muhaz
+      Made with React by Md. Shamsul Arefin
       <Link to="/">
       <button>Home?</button></Link>
     </div>
